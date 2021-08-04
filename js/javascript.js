2810d5f7149ecc2abbe05044ebb3da45d817a133
Augusto Enciso <clicona@belatec.pe>
2021-08-03 22:01:50 -0500

    cambios: fotografías, ajuste de líneas css y html, npm farallax

--- js/javascript.js
+++ js/javascript.js
@@ -26,13 +26,101 @@ $(document).ready(function() {
         btnVolverArriba.css('margin-right', '-60px');
       }
   
-    })
-
-        $('a.volver-arriba').on('click', function (e) {
-        e.preventDefault();
-        if ($(window).scrollTop() != 0) {
-        $('html, body').animate({scrollTop: 0}, 1000);
-        }
-        })
+    });
+// Volver arriba, clic
+  $('a.volver-arriba').on('click', function (e) {
+    e.preventDefault();
+      if ($(window).scrollTop() != 0) {
+      $('html, body').stop().animate({scrollTop: 0}, 1000);
+      }
+      })
   
-        });
+    });
+
+// Los demás ítems de la página
+  $('a.scroll-suave').on('click', function (e){
+    var seccionOffSetTop = $($(this).attr('href')).offset().top;
+    e.preventDefault();
+    $('html, body').stop().animate({scrollTop: seccionOffSetTop}, 1000);
+});
+
+// AcercaDe
+$('a.scroll-acercaDe').on('click', function (e){
+  var seccionOffSetTop = $($(this).attr('href')).offset().top - 122;
+  e.preventDefault();
+  $('html, body').stop().animate({scrollTop: seccionOffSetTop}, 1000);
+});
+
+// Equipo
+$('a.scroll-equipo').on('click', function (e){
+  var seccionOffSetTop = $($(this).attr('href')).offset().top - 122;
+  e.preventDefault();
+  $('html, body').stop().animate({scrollTop: seccionOffSetTop}, 1000);
+});
+
+// Modal
+$(document).ready(function () {
+  $('.imagen-proyecto').on('click', function () {
+    var rutaImagen = $(this).attr('src');
+    var modal = '<div class="modal" id="modal"><img src="'+ rutaImagen  +'" alt=""><div class="btn-cerrar" id="btnCerrar"><i class="fa fa-times" aria-hidden="true"></i></div></div>';
+    $('#proyectos').after(modal);
+    $('#btnCerrar').on('click', function () {
+      $('#modal').remove();
+    })
+  })
+
+  $(document).on('keyup', function (e) {
+    if (e.which==27) {
+      $('#modal').remove();
+    }
+  })
+
+});
+
+// Slider
+$(document).ready(function () {
+  var slider = $('#slider');
+  var btnAnterior = $('#btnAnterior');
+  var btnSiguiente = $('#btnSiguiente');
+  // El último slide pasa al primer lugar
+  $('#slider .slide:last').insertBefore('#slider .slide:first');
+  // El margen negativo se vuelve a mostrar como el primer slide
+  slider.css('margin-left', '-43%');
+  // Mover hacia la derecha
+  function moverDerecha() {
+    if (!slider.is(':animated')) {
+      slider.animate({
+        marginLeft: '-95.5%'
+      }, 700, function () {
+        $('#slider .slide:first').insertAfter('#slider .slide:last');
+        slider.css('margin-left', '-43%');
+        resetInterval();
+      });
+    }
+
+  }
+
+  btnSiguiente.on('click', moverDerecha);
+  // Hacia la izquierda
+  function moverIzquierda() {
+    if (!slider.is(':animated')) {
+      $('#slider .slide:last').insertBefore('#slider .slide:first');
+      slider.css('margin-left', '-95.5%');
+      slider.animate({
+        marginLeft: '-43%'
+      }, 700, function () {
+        resetInterval()
+      });
+    }
+
+  }
+
+  btnAnterior.on('click', moverIzquierda);
+  // Movimiento automatico
+  var intervalo = setInterval(moverDerecha, 5000);
+    function resetInterval(){
+      clearInterval(intervalo);
+      intervalo = setInterval(moverDerecha, 5000);
+  }
+
+});
